4916a548d8e734d09e46d7adbd968ccf64d7b230
Chicior9001 <Komar9001@wp.pl>
2024-09-13 01:16:38 +0200

    Add files via upload

---
+++ Nauka/Kolokwium 2/Kolokwium-2/src/test/java/com/nauka/Kolokwium2/Kolokwium2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nauka.Kolokwium2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kolokwium2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
